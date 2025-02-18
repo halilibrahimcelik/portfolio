af557386f1a2942517deb57138044e7d11076775
halilibrahimcelik <halil.celik139@gmail.com>
2025-02-18 23:56:10 +0000

    feat: mobile responsiveness added to sticky header.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -13,7 +13,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
           <ThemeToggle />
         </div>
       </div>
-      <div className='flex gap-4  mt-20 relative min-h-[140vh]'>
+      <div className='flex flex-col lg:flex-row gap-4  mt-20 relative min-h-[140vh]'>
         <StickyHeader />
         <Card asChild className='flex-grow '>
           <main>{children}</main>
