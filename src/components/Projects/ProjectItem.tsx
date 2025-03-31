cbdee05ba46ac29af6337bbd1f11a0ee4d6faf95
halilibrahimcelik <halil.celik139@gmail.com>
2025-03-31 22:35:46 +0100

    feat: enhance project item animation; implement framer-motion for smoother transitions

--- src/components/Projects/ProjectItem.tsx
+++ src/components/Projects/ProjectItem.tsx
@@ -3,36 +3,45 @@ import AnimatedCard from '../ui/AnimatedCard';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Heading } from '../theme/typography';
+import { motion } from 'framer-motion';
 
 type Props = {
   project: Project;
+  index: number;
 };
 
-const ProjectItem: React.FC<Props> = ({ project }) => {
+const ProjectItem: React.FC<Props> = ({ project, index }) => {
   return (
-    <Link
-      className='w-full h-full flex flex-col items-center justify-center'
-      href={`/projects/${project.sys.id}`}
-      passHref
+    <motion.li
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.3, ease: 'easeInOut', delay: 0.1 * index }}
     >
-      <AnimatedCard className='w-full p-3 group' circleSize={400}>
-        <Heading
-          variant='h4'
-          className='dark:group-hover:text-white relative text-center mb-2'
-        >
-          {project.title}
-        </Heading>
-        {/* <p>{project.description}</p> */}
-        <div className='h-72 overflow-hidden  relative rounded-2xl'>
-          <Image
-            src={project.image.url}
-            alt={project.title}
-            fill
-            className='object-cover group-hover:h-20   transition-all ease-in duration-150  w-full rounded-2xl'
-          />
-        </div>
-      </AnimatedCard>
-    </Link>
+      <Link
+        className='w-full h-full flex flex-col items-center justify-center'
+        href={`/projects/${project.sys.id}`}
+        passHref
+      >
+        <AnimatedCard className='w-full p-3 group' circleSize={400}>
+          <Heading
+            variant='h4'
+            className='dark:group-hover:text-white relative text-center mb-2'
+          >
+            {project.title}
+          </Heading>
+          {/* <p>{project.description}</p> */}
+          <div className='h-72 overflow-hidden  relative rounded-2xl'>
+            <Image
+              src={project.image.url}
+              alt={project.title}
+              fill
+              className='object-cover group-hover:h-20   transition-all ease-in duration-150  w-full rounded-2xl'
+            />
+          </div>
+        </AnimatedCard>
+      </Link>
+    </motion.li>
   );
 };
 export default ProjectItem;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ProjectItem from './ProjectItem';
 import { Project } from '@/lib/queries';
+import { AnimatePresence } from 'framer-motion';
 
 type Props = {
   projects: Project[];
@@ -9,10 +10,14 @@ type Props = {
 
 const Projects: React.FC<Props> = ({ projects }) => {
   return (
-    <ul className='grid  grid-cols-1  gap-4 lg:grid-cols-2'>
-      {projects.map((projects) => {
-        return <ProjectItem key={projects.sys.id} project={projects} />;
-      })}
+    <ul className='grid  grid-cols-1 my-5  gap-4 lg:grid-cols-2'>
+      <AnimatePresence mode='wait'>
+        {projects.map((project, index) => {
+          return (
+            <ProjectItem key={project.sys.id} project={project} index={index} />
+          );
+        })}
+      </AnimatePresence>
     </ul>
   );
 };
