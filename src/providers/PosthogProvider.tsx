b92e66dc11a88063ab4478b9d28c46c3539ac686
halilibrahimcelik <halil.celik139@gmail.com>
2025-04-04 21:53:36 +0100

    feat: initialize PostHog only in production environment to avoid test events in development

--- src/providers/PosthogProvider.tsx
+++ src/providers/PosthogProvider.tsx
@@ -41,21 +41,14 @@ function SuspendedPostHogPageView() {
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-    });
-    console.log(
-      'PostHog initialized with key:',
-      process.env.NEXT_PUBLIC_POSTHOG_KEY?.substring(0, 5) + '...',
-      'Host:',
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com'
-    );
-
-    // Test event
-    posthog.capture('test_event', { test: 'value' });
+    if (process.env.NODE_ENV !== 'development') {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+        api_host:
+          process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+        person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      });
+    }
   }, []);
 
   return (
